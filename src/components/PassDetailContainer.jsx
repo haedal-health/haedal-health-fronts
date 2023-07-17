989d46e770e72d51263443095186b558546aa210
DingX2 <pourding@naver.com>
2023-07-18 03:06:01 +0900

    [FEAT] detail 감추기 기능 추가

--- src/components/PassDetailContainer.jsx
+++ src/components/PassDetailContainer.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 
 import "../styles/ReservationDetails.scss";
 export default function PassDetailContainer(
@@ -8,33 +8,48 @@ export default function PassDetailContainer(
   time,
   trainer
 ) {
+  const [isExpanded, setIsExpanded] = useState(false);
+
+  const handleToggle = () => {
+    setIsExpanded(!isExpanded);
+  };
+
   return (
     <div className="ReservationDetails-container">
       <div className="ReservationDetails-title-groups align-center">
         <span className="ReservationDetails-item-title titlefont2 align-center">
           {passtypeText}
         </span>
-        <button className="ReservationDetails-item-btn btnfont1 align-center">
+        <button
+          className="ReservationDetails-item-btn btnfont1 align-center"
+          onClick={handleToggle}
+        >
           {text}
         </button>
       </div>
 
-      <div class="ReservationDetails-item-groups">
-        <ul class="ReservationDetails-item-detail-text">
-          <li>
-            <span class="item-title">날짜</span>
-            {date}
-          </li>
-          <li>
-            <span class="item-title">예약 시간</span>
-            {time}
-          </li>
-          <li>
-            <span class="item-title">담당 선생님</span>
-            {trainer} 트레이너
-          </li>
-        </ul>
-      </div>
+      {isExpanded && (
+        <div
+          className={`ReservationDetails-item-groups ${
+            isExpanded ? "expanded" : ""
+          }`}
+        >
+          <ul class="ReservationDetails-item-detail-text">
+            <li>
+              <span class="item-title">날짜</span>
+              {date}
+            </li>
+            <li>
+              <span class="item-title">예약 시간</span>
+              {time}
+            </li>
+            <li>
+              <span class="item-title">담당 선생님</span>
+              {trainer} 트레이너
+            </li>
+          </ul>
+        </div>
+      )}
     </div>
   );
 }
