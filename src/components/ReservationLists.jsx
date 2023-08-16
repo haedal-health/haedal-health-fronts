0a2283488d27aecd10273becddb8bdaf69832956
DingX2 <96682768+DingX2@users.noreply.github.com>
2023-08-17 02:48:03 +0900

    [FEAT] pass 연결 완료
    - pass component 연결
    - name 으로 이용권 리스트 만들기
    - 리액트에선 size 수정 불가능

--- src/components/ReservationLists.jsx
+++ src/components/ReservationLists.jsx
@@ -7,70 +7,21 @@ import {
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import "../styles/ReservationLists.scss";
 import renderPassType from "./PassType";
+import JsonPassResource from "../apis/axiosPassApi";
+
+const pass = await JsonPassResource.fetchPassContent();
+console.log("pass.name");
+console.log(pass);
 
 export default function ReservationLists() {
   const URL = "booking/";
-  const [pass, setPass] = useState([]);
+  const [passes, setPass] = useState([]);
   const [loading, setLoading] = useState(false);
-  const [bookingData, setBookingData] = useState([]);
-  const [bookingDataAry, setBookingDataAry] = useState([]);
 
   useEffect(() => {
-    // fetchData();
-    // fetchDataPass();
-    // console.log(bookingData);
-  }, [loading]);
-
-  async function fetchDataPass() {
-    console.log("Fetching data...");
-    try {
-      const instance = axios.create({
-        baseURL: "/booking",
-        withCredentials: true,
-      });
-
-      const response = await axios.get("/pass?page=1&size=4&sort=startedDay", {
-        withCredentials: true,
-      });
-
-      if (!response.data) {
-        throw new Error("Failed to fetch data from the server");
-      }
-      setPass((predata) => response.data);
-      console.log("pass");
-      console.log(pass);
-      setLoading((preloading) => true);
-    } catch (error) {
-      console.error(error);
-    }
-  }
-
-  async function fetchData() {
-    console.log("Fetching data...");
-    try {
-      const response = await axios.get(URL, {
-        withCredentials: true,
-      });
-
-      if (!response.data) {
-        throw new Error("Failed to fetch data from the server");
-      }
-      const data = response.data.content;
-      setBookingData((predata) => response.data);
-      setBookingDataAry((predata) => response.data.content);
-      // console.log("bookingDataAry check");
-      // console.log(bookingDataAry);
-      setLoading((preloading) => true);
-    } catch (error) {
-      console.error(error);
-    } ///
-  }
-
-  const passTypeName1 = "헬스 이용권 1";
-  const passTypeName2 = "헬스 이용권 2";
-  const passTypeName3 = "헬스 이용권 3";
-  const passTypeName4 = "헬스 이용권 4";
-  const passTypeName5 = "헬스 이용권 5";
+    setPass((pre) => pass);
+    console.log(passes);
+  }, []);
 
   return (
     <div className="ReservationList-wrapper">
@@ -83,11 +34,9 @@ export default function ReservationLists() {
       </button>
 
       <div className="ReservationList-container">
-        {renderPassType(passTypeName1)}
-        {renderPassType(passTypeName2)}
-        {renderPassType(passTypeName3)}
-        {renderPassType(passTypeName4)}
-        {renderPassType(passTypeName5)}
+        {pass.map((item, index) => (
+          <div key={index}>{renderPassType(item.name)}</div>
+        ))}
       </div>
     </div>
   );
